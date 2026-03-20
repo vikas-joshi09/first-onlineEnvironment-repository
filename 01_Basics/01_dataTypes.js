9fa65ac2f0a4040b90312e9f9e26b5155ff10b5f
Vikas Joshi <165507575+vikas-joshi09@users.noreply.github.com>
2026-03-20 17:37:51 +0000

    datatype file commited

---
+++ 01_Basics/01_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all js code as newer version
+
+// alert("Hello World! This side Vikas"); we are using node js, not browser;
+
+let name = "vikas";
+let age = 24;
+let isLoggedIn = false;
+
+
+// ------Premetive----------
+//number 
+//bigint
+//string
+//boolean
+//null
+//undefined
+//symbol
+
+//Non-Premetive---------
+//object
+//array
+
+
+// "33" convert into number its convert--33
+//"33abc" convert into number its convert and type is showing number but if you print variable its showing NAN 
+// 1=true; 0=false;
+
+
--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,1 +1,22 @@
 console.log("vikas");
+
+var name = "vikas";
+console.log(name);
+
+const accountId = 133434;
+let accountEmail = "vikas@gmail.com";
+var accountPassword = "12345";
+let accountCity;
+
+// prefered not to use var because of its issue with block or functional scope.
+accountEmail = "vicky@gmail.com";
+accountPassword = "2222";
+accountCity = "delhi";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+
+// -------Data Types of JavaScript----------
+
+
+
